d097e0a492dd9b1692ab94f34c6a43e7b800063c
Elxan Atayev <elxanatayev.la@gmail.com>
2021-07-09 12:16:01 +0400

    Add project files.

---
+++ Text Editor Home Work/BUC(Button User Control).xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Text_Editor_Home_Work
+{
+    /// <summary>
+    /// Interaction logic for BUC_Button_User_Control_.xaml
+    /// </summary>
+    public partial class BUC_Button_User_Control_ : UserControl
+    {
+        public bool Toggled1 { get => Toggled; set => Toggled = value; }
+        Thickness LeftSide = new Thickness(-39, 0, 0, 0);
+        Thickness RightSide = new Thickness(0, 0, -39, 0);
+        SolidColorBrush Off = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+        SolidColorBrush On = new SolidColorBrush(Color.FromRgb(130, 190, 125));
+        private bool Toggled = false;
+        public BUC_Button_User_Control_()
+        {
+            InitializeComponent();
+            Back.Fill = Off;
+            Toggled = false;
+            Dot.Margin = LeftSide;
+        }
+        private void Dot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!Toggled)
+            {
+                Back.Fill = On;
+                Toggled = true;
+                Dot.Margin = RightSide;
+            }
+            else
+            {
+                Back.Fill = Off;
+                Toggled = false;
+                Dot.Margin = LeftSide;
+            }
+        }
+        private void Back_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!Toggled)
+            {
+                Back.Fill = On;
+                Toggled = true;
+                Dot.Margin = RightSide;
+            }
+            else
+            {
+                Back.Fill = Off;
+                Toggled = false;
+                Dot.Margin = LeftSide;
+            }
+        }
+    }
+}
